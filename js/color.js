f5319fc6380f022c337357be384ec047e50bab5b
Marie Leidinger <marielei@gmx.de>
2014-10-13 22:54:17 +0200

    Update2

--- js/color.js
+++ js/color.js
@@ -1,20 +1,46 @@
         $('body').colorScroll({
             colors: [{
-                color: '#a2e9ff',
+                color: '#c3a0a5',
                 position: '0%'
             }, {
-                color: '#9aff61',
+                color: '#927ca5',
                 position: '20%'
             }, {
-                color: '#aaff9e',
+                color: '#f7a0bc',
                 position: '40%'
             }, {
-                color: '#ffa378',
+                color: '#f7d8a8',
                 position: '60%'
             }, {
-                color: '#92b3ff',
+                color: '#adf7c3',
                 position: '80%'
+            }, {
+                color: '#a1bbf7',
+                position: '100%'
             }]
         });
+
+
+
+
+
+        //      $('body').colorScroll({
+        //     colors: [{
+        //         color: '#a2e9ff',
+        //         position: '0%'
+        //     }, {
+        //         color: '#9aff61',
+        //         position: '20%'
+        //     }, {
+        //         color: '#aaff9e',
+        //         position: '40%'
+        //     }, {
+        //         color: '#ffa378',
+        //         position: '60%'
+        //     }, {
+        //         color: '#92b3ff',
+        //         position: '80%'
+        //     }]
+        // });
     
 
