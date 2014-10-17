73ee89c2272b76fa6147a278a7c5b3ee797edbe4
Marie Leidinger <marielei@gmx.de>
2014-10-17 02:51:49 +0200

    Update final

--- js/color.js
+++ js/color.js
@@ -1,29 +1,30 @@
-        $('body').colorScroll({
+        $('#page-color').colorScroll({
             colors: [{
-                color: '#bfb1ab',
+                color: '#dfe1e1 ',
                 position: '0%'
             }, {
+                color: '#bfb1ab',
+                position: '13%'
+            }, {
                 color: '#c3a0a5',
-                position: '20%'
+                position: '25%'
             }, {
                 color: '#d78e94',
-                position: '40%'
+                position: '38%'
             }, {
                 color: '#ad99d6',
-                position: '60%'
+                position: '50%'
             }, {
                 color: '#b5d6b7',
-                position: '80%'
+                position: '63%'
             }, {
                 color: '#abd6be',
-                position: '90%'
+                position: '78%'
             }, {
                 color: '#efe7cb',
                 position: '100%'
             }]
         });
-
-
 
 
 
