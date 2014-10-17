5d063f66e170a894b7683522d703b15bbb9c2396
Marie Leidinger <marielei@gmx.de>
2014-10-17 03:20:33 +0200

    Update 17.10.

--- js/color.js
+++ js/color.js
@@ -28,24 +28,32 @@
 
 
 
-        //   #b5d6b7
-        //   $('body').colorScroll({
-        //     colors: [{
-        //         color: '#a2e9ff',
-        //         position: '0%'
-        //     }, {
-        //         color: '#9aff61',
-        //         position: '20%'
-        //     }, {
-        //         color: '#aaff9e',
-        //         position: '40%'
-        //     }, {
-        //         color: '#ffa378',
-        //         position: '60%'
-        //     }, {
-        //         color: '#92b3ff',
-        //         position: '80%'
-        //     }]
-        // });
+        $('.mask').colorScroll({
+            colors: [{
+                color: '#dfe1e1 ',
+                position: '0%'
+            }, {
+                color: '#bfb1ab',
+                position: '13%'
+            }, {
+                color: '#c3a0a5',
+                position: '25%'
+            }, {
+                color: '#d78e94',
+                position: '38%'
+            }, {
+                color: '#ad99d6',
+                position: '50%'
+            }, {
+                color: '#b5d6b7',
+                position: '63%'
+            }, {
+                color: '#abd6be',
+                position: '78%'
+            }, {
+                color: '#efe7cb',
+                position: '100%'
+            }]
+        });
     
 
